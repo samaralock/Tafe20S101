9f8ed16b259944b2d3ca88c7b932f3bf24fde874
Samara Lock <locksamara@gmail.com>
2020-05-04 12:16:07 +0930

    Second commit of Contact Details

---
+++ StartFinanceMaster/InstaRichie/Models/Contact.cs
@@ -0,0 +1,23 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartFinance.Models
+{
+    class Contact
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ContactID { get; set; }
+        [NotNull]
+        public string ContactFirstName { get; set; }
+        [NotNull]
+        public string ContactLastName { get; set; }
+        [NotNull]
+        public string ContactCompany { get; set; }
+        [NotNull]
+        public string ContactPhone { get; set; }
+    }
+}
